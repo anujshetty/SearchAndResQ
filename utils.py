3745fe0835c99f81ae3efe895b57836ffd9741eb
Amrita Palaparthi <amritapv@stanford.edu>
2023-03-11 20:46:33 -0800

    adding beefed up visualization, modular simulation code

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 #Utility functions for the gridworld environment
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+import time
+from IPython import display
+import random
 
 # Visualization helper functions
 def chars_to_num(char_grid):
@@ -36,23 +39,38 @@ def chars_to_icons(char_grid):
     icon_grid[icon_grid == 'D'] = '🧗'
     return icon_grid
 
-def visualize_grid(char_grid):
+def visualize_grid(g):
     """
     Visualize a 2D grid of characters in matplotlib with emojis
     """
+    char_grid = g.gridworld_to_arr()
     colors = ['saddlebrown', 'red', 'green', 'yellow']
     cmap = ListedColormap(colors)
     fig, ax = plt.subplots()
     num_grid = chars_to_num(char_grid)
     icon_grid = chars_to_icons(char_grid)
-
+    orientations = ['0', '90', '180', '270']   
+    
     for y in range(char_grid.shape[0]):
-       for x in range(char_grid.shape[1]):
-          plt.text(x , y, icon_grid[y, x],
-             horizontalalignment='center',
-             verticalalignment='center',
-             fontname='Segoe UI Emoji'
-          )
+        for x in range(char_grid.shape[1]):
+
+            if icon_grid[y, x] == '🤖':
+                plt.text(x , y, f" {icon_grid[y, x]} ",
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    rotation=orientations[g.getOrientation()],
+                    rotation_mode='anchor',
+                    fontname='Segoe UI Emoji'
+                ) 
+            else:
+                plt.text(x , y, icon_grid[y, x],
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    fontname='Segoe UI Emoji'
+                )
+    
+    
+    
     ax.matshow(num_grid, cmap=cmap, vmin=0, vmax=len(colors))
     
     
@@ -65,3 +83,37 @@ def policy_score(rewards, discount_factor):
     for i in range(len(rewards)):
         score += (discount_factor**i)*rewards[i]
     return score
+
+def simulate_policy(g, policy_type, run_to_completion=True, num_iters=0, policy=None):
+    """
+    Simulates a run of specified policy in a given gridworld g for either num_iters iterations or until target is reached.
+    Returns discounted sum of rewards for the input policy.
+    Policy type can either be "random" (which auto-generates actions) or "fixed" (which uses the provided policy dictionary)
+    policy dictionary should contain mappings (s) -> (a) given states
+    THIS FUNCTION CURRENTLY ONLY SUPPORTS A RANDOM POLICY
+    """
+    rewards=[]
+    def simulate_iteration(rewards):
+
+        visualize_grid(g)
+        display.display(plt.gcf())
+        display.clear_output(wait=True)
+        time.sleep(0.2)
+        # choose a random action
+        if policy_type=="random":
+            action = random.choice(g.actions[0])
+        #print(f'Taking action: {action}')
+        # take the action and update the state
+        reward = g.takeAction(action)
+        #print("Action: ", action, "Reward: ", reward)
+        rewards.append(reward) 
+
+    if run_to_completion:
+        while g.getCoords() != g.destination:
+            simulate_iteration(rewards)
+    else:
+        for _ in range(num_iters):
+            simulate_iteration(rewards)
+    plt.close("all")
+    return policy_score(rewards, g.gamma)
+ 
